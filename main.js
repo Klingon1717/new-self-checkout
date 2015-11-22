90ce615ca04b8fbd49ddd08db6898a1179a3882a
Whitney Murray <wnksmith92@yahoo.com>
2015-11-22 17:54:51 -0600

    finished

---
+++ main.js
@@ -0,0 +1,18 @@
+var priceOne = prompt('what is the price of your first item');
+var quantOne = prompt('what quanity did you want of this item');
+console.log(priceOne + ' ' + quantOne);
+var itemOne =(priceOne * quantOne);
+var priceTwo = prompt('what is the price of your second item');
+var quantTwo = prompt('what quanity did you want of this item');
+console.log(priceTwo + ' ' + quantTwo);
+var itemTwo =( priceTwo * quantTwo);
+var priceThree = prompt('what is the price of your third item');
+var quantThree = prompt('what quanity did you want of this item');
+console.log(priceThree + ' ' + quantThree);
+var itemThree =(priceThree * quantThree);
+
+var sub = itemOne + itemTwo + itemThree;
+console.log(sub);
+var tax = Math.round(0.055 * 100)/100;
+var total =Math.round(sub * 1.055 *100)/100;
+console.log('\$' + total);
